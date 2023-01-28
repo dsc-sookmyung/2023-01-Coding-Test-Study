8eb7979df6869ab1417b7d285ba67201717ef965
bnfkim <kgr3819@gmail.com>
2023-01-29 01:10:54 +0900

    [week2] DataStructure2 - 14425

---
+++ 02-DataStructure2/bnfkim/14425-문자열집합.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 0);
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (map.containsKey(br.readLine())) count++;
+        }
+        System.out.print(count);
+    }
+}
