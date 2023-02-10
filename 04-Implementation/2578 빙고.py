00a0fa0e43fde4442f8f1bff7c8ae9f9d3fd599a
mingadinga <minpearl0826@gmail.com>
2023-02-11 00:56:24 +0900

    [week4] - Implementation 빙고 코드 수정

--- 04-Implementation/2578 빙고.py
+++ 04-Implementation/2578 빙고.py
@@ -1,17 +1,13 @@
 def check_call(target):
-  print("call : ")
-  for i in range(5):
-    for j in range(5):
-      if bingo[i][j] == target:
-        bingo[i][j] = 0
-
-    print(bingo[i])
+    for i in range(5):
+        for j in range(5):
+            if bingo[i][j] == target:
+                bingo[i][j] = 0
 
 def is_bingo_three():
     bingo_count = 0
     for row in bingo:
         if [0, 0, 0, 0, 0] == row:
-            print("row bingo!")
             bingo_count += 1
 
     for column in range(5):
@@ -19,7 +15,6 @@ def is_bingo_three():
         for index in range(5):
             bingo_column.append(bingo[index][column])
         if [0, 0, 0, 0, 0] == bingo_column:
-            print("column bingo!")
             bingo_count += 1
 
     diagonal_left, diagonal_right = [], []
@@ -27,22 +22,30 @@ def is_bingo_three():
         diagonal_left.append(bingo[i][i])
         diagonal_right.append(bingo[i][4 - i])
     if [0, 0, 0, 0, 0] == diagonal_left:
-        print("diagnal bingo!")
         bingo_count += 1
     if [0, 0, 0, 0, 0] == diagonal_right:
-        print("diagnal bingo!")
         bingo_count += 1
 
-    print(bingo_count)
     if bingo_count >= 3:
         return True
     return False
 
-bingo = [[11, 12, 2, 24, 10], [16, 1, 13, 3, 25], [6, 20, 5, 21, 17], [19, 4, 8, 14, 9], [22, 15, 7, 23, 18]]
-calls = [5, 10, 7, 16, 2, 4, 22, 8, 17, 13, 3, 18, 1, 6, 25, 12, 19, 23, 14, 21, 11, 24, 9, 20, 15]
+
+import sys
+
+read = sys.stdin.readline
+
+bingo = []
+calls = []
+
+for _ in range(5):
+    bingo.append(list(map(int, read().split())))
+for _ in range(5):
+    calls.append(list(map(int, read().split())))
+calls = sum(calls, [])
 
 for call in calls:
-  check_call(call)
-  if is_bingo_three():
-    print(calls.index(call) + 1)
-    break
+    check_call(call)
+    if is_bingo_three():
+        print(calls.index(call) + 1)
+        break
